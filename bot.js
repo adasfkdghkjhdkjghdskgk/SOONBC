efc15640ad3dae4e1f6721499b9a121492adf018
adasfkdghkjhdkjghdskgk <44849612+adasfkdghkjhdkjghdskgk@users.noreply.github.com>
2018-11-18 22:53:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -111,11 +111,10 @@ client.on('message', message => {
     if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
 if (message.content.startsWith('$$$$bc')){
-message.channel.sendMessage('جار ارسال الرسالة |✅')
 if(!message.author.id === '380650390657695744') return;
 if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
   if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
-    let copy = "Speed Bot";
+message.channel.sendMessage('جار ارسال الرسالة |✅')
 client.users.forEach(m =>{
 m.sendMessage(args)
 })
